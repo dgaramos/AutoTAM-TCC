9faf50e90f5f56adbd632a5e9883ac18fb1aaf9a
Danilo Ramos <dgaramos@gmail.com>
2016-09-04 01:36:35 -0300

    autenticação aparentemente resolvida, cada classe foi documentada, ainda não foram documentados os métodos de cada classe

---
+++ build/resources/main/static/js/angular/app.js
@@ -0,0 +1,2 @@
+'use strict';
+var myApp = angular.module('clientAutoTAM', ['ngRoute']);
---
+++ build/resources/main/static/js/angular/configs/routes.js
@@ -0,0 +1,21 @@
+'use strict';
+myApp.config(function ($routeProvider, $httpProvider){
+        $routeProvider.
+        when('/ListarUsuarios', {
+          templateUrl: 'listausuarios.html',
+          controller: 'UsuarioController'
+          
+        }).
+        when('/Login', {
+          templateUrl: 'login.html',
+          controller: 'LoginController',
+            controllerAs: 'ctrl'
+
+        }).
+        otherwise({
+            redirectTo: '/Login'
+        });
+
+      $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    })
+
