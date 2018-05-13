0a292747aaea8f7afcf3d83794855e8531b569f4
Danilo Ramos <dgaramos@gmail.com>
2018-05-13 14:24:00 -0300

    configurando para funcionar online no heroku

--- src/main/resources/static/app/env.js
+++ src/main/resources/static/app/env.js
@@ -26,7 +26,7 @@
     // API url
     // For demo purposes we fetch from local file in this plunk
     // In your application this can be a url like https://api.github.com
-    window.__env.apiUrl = 'http://localhost:9000';
+    window.__env.apiUrl = 'http://autotam-tcc.herokuapp.com';
 
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
